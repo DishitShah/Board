cc4d86fcd52b3f89d1f6ae044974b733c99996c9
DishitShah <119598939+DishitShah@users.noreply.github.com>
2025-03-02 07:32:42 +0530

    Add files via upload

---
+++ convex/auth.config.js
@@ -0,0 +1,9 @@
+export default {
+  providers: [
+    {
+      domain: "https://clerk.rushabhshah.live",
+      applicationID: "convex",
+    },
+  ],
+};
+//"https://clerk.rushabhshah.live", "https://optimum-pheasant-56.clerk.accounts.dev"
